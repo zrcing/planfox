50f81fde955a84c5414abeded79b2c6a0fc5e343
Gary <liaogling@gmail.com>
2017-03-30 13:02:21 +0800

    Exception

---
+++ Exception/Exception.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Liao Gengling <liaogling@gmail.com>
+ */
+namespace Planfox\Exception;
+
+class Exception extends \Exception
+{
+
+}
---
+++ Exception/InvalidArgumentException.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Liao Gengling <liaogling@gmail.com>
+ */
+namespace Planfox\Exception;
+
+class InvalidArgumentException extends \InvalidArgumentException
+{
+
+}
